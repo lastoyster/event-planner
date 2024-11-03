f4fb9615d930598376dbc00d8ac690288b63c559
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2024-11-03 11:02:35 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+import { NhostClient, NhostReactProvider } from "@nhost/react";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+import * as SecureStore from "expo-secure-store";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+const nhost = new NhostClient({
+  subdomain: "mnjdtroppbmdosupvetf",
+  region: "eu-central-1",
+  clientStorageType: "expo-secure-storage",
+  clientStorage: SecureStore,
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaProvider>
+          <NhostReactProvider nhost={nhost}>
+            <NhostApolloProvider nhost={nhost}>
+              <Navigation colorScheme={colorScheme} />
+            </NhostApolloProvider>
+          </NhostReactProvider>
+          <StatusBar />
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
+    );
+  }
+}
